52803e5c6f4bdc62cf052bff09ce26bccc2d2c95
sivanitski <serhiiivan2@gmail.com>
2022-05-19 16:11:31 +0600

    fix: updated api response statuses

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -9,7 +9,7 @@ class GeolocationsController < ApplicationController
         if geolocation_data
           @geolocation = Geolocation.new(geolocation_data)
           if @geolocation.save
-            render json: serialized('geolocation', @geolocation, { include: [:location] }), status: 201
+            render json: serialized('geolocation', @geolocation, { include: [:location] }), status: :created
           else
             render json: { error: 'Geolocatio could not be saved!!' }, status: :unprocessable_entity
           end
@@ -17,26 +17,26 @@ class GeolocationsController < ApplicationController
           render json: { error: 'Something went wrong!!' }, status: :unprocessable_entity
         end
       rescue StandardError => e
-        render json: { error: e }, status: 422
+        render json: { error: e }, status: :unprocessable_entity
       end
     else
-      render json: serialized('geolocation', @geolocation, { include: [:location] }), status: 302
+      render json: serialized('geolocation', @geolocation, { include: [:location] }), status: :found
     end
   end
 
   def show
     @geolocation = Geolocation.find_by(ip: @ip_address)
     if @geolocation.present?
-      render json: serialized('geolocation', @geolocation, { include: [:location] }), status: 302
+      render json: serialized('geolocation', @geolocation, { include: [:location] }), status: :ok
     else
-      render json: { error: 'Could not find geolocation with the IP' }, status: 404
+      render json: { error: 'Could not find geolocation with the IP' }, status: :not_found
     end
   end
 
   def destroy
     @geolocation = Geolocation.find_by(ip: @ip_address)
     if @geolocation&.destroy
-      render json: { message: 'Geolocation destroyed successfully' }, status: 200
+      render json: { message: 'Geolocation destroyed successfully' }, status: :ok
     else
       render json: { error: 'Geolocation could not be destroyed!!' }, status: :unprocessable_entity
     end
